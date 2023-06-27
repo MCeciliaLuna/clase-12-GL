1be9bb68abbcb9a8df3ba75a791caba86dc8df57
MCeciliaLuna <lunama.cecilia@live.com>
2023-06-27 12:49:16 -0300

    maqueta de ejercicio lista

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+function App () {
+  let abortController
+
+  fetchData()
+
+  const fetchData = async () => {
+    abortController = new AbortController()
+
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        signal: abortController.signal
+      })
+
+      if (!response.ok) {
+        console.log('Error al realizar la solicitud')
+        return
+      }
+
+      setData(response)
+    } catch (error) {
+      console.error('Error al realizar la solicitud:', error.message)
+      setError('Error al cargar los datos. Por favor, intenta nuevamente.')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Cargando datos...</p>
+      ) : (
+        <div>
+          {error ? (
+            <p>{error}</p>
+          ) : (
+            <div>
+              <h1>Publicaciones:</h1>
+              <ul>
+                {data.map(post => (
+                  <li key={post.id}>{post.title}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
